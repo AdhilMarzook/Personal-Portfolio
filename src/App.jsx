4d983b598ba5567458891153799f6dd73987a394
AdhilMarzook <adhilmarzook20@gmail.com>
2024-02-10 18:02:14 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
 
-
+//app main
 const App = () =>{
 
   return (
